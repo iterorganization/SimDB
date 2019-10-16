673fa8957c0c25cbd9f21f763d63e630e69ff6e4
Andrew Lahiff <andrew.lahiff@ukaea.uk>
2019-10-16 13:50:37 +0100

    Adding simdb.config to packages

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     author="Jonathan Hollocombe",
     author_email="jonathan.hollocombe@ukaea.uk",
     url="https://git.iter.org/projects/IMEX/repos/simulation-management/browse",
-    packages=["simdb", "simdb.cli", "simdb.database"],
+    packages=["simdb", "simdb.cli", "simdb.database", "simdb.config"],
     license="See LICENCE.txt",
     install_requires=[
         "argcomplete (>= 1.9.4)",
