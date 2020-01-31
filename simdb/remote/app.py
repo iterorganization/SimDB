cc63699f0d6827bbd240d1b62fb0b3fdf24140d4
Hollocombe, Jonathan <jonathan.hollocombe@ukaea.uk>
2020-01-31 14:40:35 +0000

    adding scripts and updating app.py to run with nginx/gunicorn

--- simdb/remote/app.py
+++ simdb/remote/app.py
@@ -1,24 +1,23 @@
 from flask import Flask, jsonify, request
 import ssl
 import os
+import appdirs
 
 from .api import api
 from .. import __version__
 
+dir_path = appdirs.user_config_dir('simdb')
+
+app = Flask(__name__)
+app.config.from_pyfile(os.path.join(dir_path, "app.cfg"))
+
+@app.route("/")
+def index():
+    return jsonify({"urls": [ request.url + "api/v" + __version__  ]})
+
+app.register_blueprint(api, url_prefix="/api/v" + __version__)
 
 def run():
-    import appdirs
-    dir_path = appdirs.user_config_dir('simdb')
-
-    app = Flask(__name__)
-    app.config.from_pyfile(os.path.join(dir_path, "app.cfg"))
-
-    @app.route("/")
-    def index():
-        return jsonify({"urls": [ request.url + "api/v" + __version__  ]})
-
-    app.register_blueprint(api, url_prefix="/api/v" + __version__)
-
     if app.config['SSL_ENABLED']:
         context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
         context.load_cert_chain(os.path.join(dir_path, "server.crt"), keyfile=os.path.join(dir_path, "server.key"))
