41a4e1c19435808a173a9289e17ec5b6e82c7e39
Jonathan Hollocombe <jonathan.hollocombe@ccfe.ac.uk>
2021-03-16 16:50:57 +0100

    Updating ITER validation rules and adding custom validation methods.

--- src/simdb/validation/validator.py
+++ src/simdb/validation/validator.py
@@ -46,7 +46,7 @@ class CustomValidator(cerberus.Validator):
         """
         if not isinstance(value, numpy.ndarray):
             self._error(field, "Value is not a numpy array")
-        if min_value and value.min() < min_value:
+        if min_value is not None and value.min() < min_value:
             self._error(field, "Minimum %s less than %s" % (value.min(), min_value))
 
     def _validate_max_value(self, max_value, field, value):
@@ -55,8 +55,44 @@ class CustomValidator(cerberus.Validator):
         """
         if not isinstance(value, numpy.ndarray):
             self._error(field, "Value is not a numpy array")
-        if max_value and value.min() > max_value:
+        if max_value is not None and value.max() > max_value:
             self._error(field, "Maximum %s greater than %s" % (value.max(), max_value))
+
+    def _compare(self, comparison, field, value, comparator: str, message: str):
+        if comparison is None:
+            return
+        if isinstance(value, numpy.ndarray):
+            if not getattr(value, comparator)(comparison).all():
+                self._error("Values are not %s %s" % (message, comparison))
+        elif isinstance(value, float):
+            if not getattr(value, comparator)(comparison):
+                self._error("Value is not %s %s" % (message, comparison))
+        else:
+            self._error(field, "Value is not a numpy array or a float")
+
+    def _validate_gt(self, comparison, field, value):
+        """The rule's arguments are validated against this schema:
+        {'type': 'float'}
+        """
+        self._compare(comparison, field, value, '__gt__', 'greater than')
+
+    def _validate_ge(self, comparison, field, value):
+        """The rule's arguments are validated against this schema:
+        {'type': 'float'}
+        """
+        self._compare(comparison, field, value, '__ge__', 'greater than or equal to')
+
+    def _validate_lt(self, comparison, field, value):
+        """The rule's arguments are validated against this schema:
+        {'type': 'float'}
+        """
+        self._compare(comparison, field, value, '__lt__', 'less than')
+
+    def _validate_le(self, comparison, field, value):
+        """The rule's arguments are validated against this schema:
+        {'type': 'float'}
+        """
+        self._compare(comparison, field, value, '__le__', 'less than or equal to')
 
     @classmethod
     def _normalize_coerce_int(cls, value):
