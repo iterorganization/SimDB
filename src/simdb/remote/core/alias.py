7e97e588569b968de2c0857c4f5a850febe9292c
Jonathan Hollocombe <jonathan.hollocombe@ukaea.uk>
2023-08-04 13:28:12 +0200

    Cleaning up alias folder creation code

--- src/simdb/remote/core/alias.py
+++ src/simdb/remote/core/alias.py
@@ -11,15 +11,10 @@ def create_alias_dir(simulation):
 
     alias_path = os.path.join(base_dir, "aliases", simulation.alias)
     if not os.path.exists(alias_path):
-        if "/" not in simulation.alias:
-            os.symlink(os.path.join(base_dir, simulation.uuid.hex), alias_path)
-        else:
+        if "/" in simulation.alias:
             pieces = simulation.alias.split("/")
-            last_bit = pieces[len(pieces) - 1]
-            pieces.remove(last_bit)
-            first_bit = base_dir + "/aliases/" + "/".join(pieces)
+            pieces = pieces[:-1]
+            first_bit = os.path.join(base_dir, "aliases", *pieces)
             os.makedirs(first_bit, exist_ok=True)
-            os.symlink(
-                os.path.join(base_dir, simulation.uuid.hex),
-                os.path.join(base_dir, "aliases", simulation.alias),
-            )
+
+        os.symlink(os.path.join(base_dir, simulation.uuid.hex), alias_path)
