30c8326ad898a3f09b5e30acabc7138a65ef04eb
marood <deepakkumar.maroo@iter.org>
2024-07-02 14:07:05 +0200

    Added CI plan script

---
+++ ci/build_docs.sh
@@ -0,0 +1,30 @@
+et up ITER modules environment
+set -e
+
+# Set up environment
+. /usr/share/Modules/init/sh
+module use /work/imas/etc/modules/all
+module purge
+module load Python/3.8.6-GCCcore-10.2.0
+module list
+
+# Set up virtualenv
+rm -rf venv
+python3 -m venv venv
+source venv/bin/activate
+pip3 install --upgrade pip
+pip3 install -r dev_requirements.txt
+
+# Install simdb
+pip3 install .
+
+# Build docs
+cd docs
+sphinx-apidoc -f -o sphinx -e -M ../src/simdb && rm sphinx/modules.rst
+cp *.md sphinx
+cp *.svg sphinx
+make clean
+make html
+mv _build/html/ ../html/
+# cd ..
+# tar czf html.tar.gz html/
